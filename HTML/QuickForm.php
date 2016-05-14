98dccc8dbca1374d84a235985b9e057083aee2fc
Mattias Michaux <mattias.michaux@gmail.com>
2016-05-14 12:29:24 +0200

    typo fix.

--- HTML/QuickForm.php
+++ HTML/QuickForm.php
@@ -2093,7 +2093,7 @@ class HTML_QuickForm_Error extends PEAR_Error {
         if (is_int($code)) {
             parent::__construct(HTML_QuickForm::errorMessage($code), $code, $mode, $level, $debuginfo);
         } else {
-            parent::__construct(("Invalid error code: $code", QUICKFORM_ERROR, $mode, $level, $debuginfo);
+            parent::__construct("Invalid error code: $code", QUICKFORM_ERROR, $mode, $level, $debuginfo);
         }
     }
 
